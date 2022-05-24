6710d838bd3e96dbad348fb4d87011b19d277905
Gui Rodrigues <guirocrpe@gmail.com>
2022-05-24 18:09:42 +0800

    Added collection zip download

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -1,6 +1,8 @@
 import { createDistribution, randomIndex } from '@/features/mathUtils';
+import { saveAs } from 'file-saver';
 import Asset from 'interfaces/Asset';
 import Trait from 'interfaces/Trait';
+import JSZip from 'jszip';
 import { LegacyRef, useRef, useState } from 'react';
 import { useStore } from 'react-redux';
 import Button from './buttons/Button';
@@ -11,6 +13,9 @@ export default function Canvas() {
   const store = useStore<{ traits: Trait[]; assets: Asset[] }>();
   const [rarity, setRarity] = useState<number>(0);
   const [traits, setTraits] = useState<Trait[]>(store.getState().traits);
+  const [collectionLength, setCollectionLength] = useState<number>(100);
+  const blobs = [];
+  let genIndex = 0;
 
   async function drawImage(
     context: CanvasRenderingContext2D,
@@ -40,37 +45,91 @@ export default function Canvas() {
   });
 
   function generate() {
-    if (canvas.current) {
-      const ctx = canvas.current.getContext('2d');
-      if (!ctx) return;
-      ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
-      let _rarity = 0;
+    if (!canvas?.current) return;
+    if (
+      genIndex > collectionLength ??
+      traits.length ^ store.getState().assets.length
+    ) {
+      setTimeout(() => {
+        downloadCollection();
+      }, 1000);
+      return;
+    }
+    const ctx = canvas.current.getContext('2d');
+    if (!ctx) return;
+    ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
+    let _rarity = 0;
 
-      [...store.getState().traits]
-        .sort((a, b) => b.index - a.index)
-        .forEach(async trait => {
-          const assets = store
-            .getState()
-            .assets.filter(a => a.traitId === trait.id);
+    [...store.getState().traits]
+      .sort((a, b) => b.index - a.index)
+      .forEach(async trait => {
+        const assets = store
+          .getState()
+          .assets.filter(a => a.traitId === trait.id);
 
-          const common = Math.max.apply(
-            null,
-            assets.map(a => a.percentage),
-          );
-          const distribution = createDistribution(
-            assets,
-            assets.map(a => a.percentage * 100),
-            1000,
-          );
+        const common = Math.max.apply(
+          null,
+          assets.map(a => a.percentage),
+        );
+        const distribution = createDistribution(
+          assets,
+          assets.map(a => a.percentage * 100),
+          1000,
+        );
 
-          const randomPick = assets[randomIndex(distribution)];
-          _rarity += 1 - randomPick.percentage / common;
-          console.log(common, randomPick.percentage, _rarity);
-          await drawImage(ctx, randomPick.src, 0, 0);
-        });
-      setRarity(_rarity * 100);
+        const randomPick = assets[randomIndex(distribution)];
+        _rarity += 1 - randomPick.percentage / common;
+        console.log(common, randomPick.percentage, _rarity);
+        await drawImage(ctx, randomPick.src, 0, 0);
+      });
+    setRarity(_rarity * 1000);
+    setTimeout(() => {
+      canvas.current?.toBlob(blob => {
+        addToCollection(blob);
+        genIndex++;
+        generate();
+      });
+    }, 200);
+  }
+
+  //User should be able to generate a collection of nb of assets * nb of traits NFTs
+  // and download the collection as a .zip file
+  // Each NFT should have it's own image and a json file with the following structure:
+  // {
+  //   "name": "Trait Name",
+  //   "description": "Trait Description",
+  //   "rarity": "Trait Rarity",
+  //   "traitId": "Trait Id",
+  //   "traitIndex": "Trait Index",
+  //   "image": "Trait Image"
+  // }
+  // The zip file should be named with the current date and time
+  // The zip file should contain the images of the NFTs
+  // The zip file should contain the json files of the NFTs
+  const zip = new JSZip();
+
+  function addToCollection(b: Blob) {
+    if (b) {
+      zip.file(genIndex + '.jpeg', b);
+      blobs.push(b);
     }
   }
+
+  function downloadCollection() {
+    const date = new Date();
+    const dateString = `${date.getFullYear()}-
+    ${date.getMonth()}-${date.getDate()}`;
+    const zipName = `${dateString}-collection.zip`;
+    const zipPath = `${dateString}-collection.zip`;
+
+    setTimeout(() => {
+      const zipFile = zip.generateAsync({ type: 'blob' });
+      zipFile.then(blob => {
+        saveAs(blob, zipName);
+      });
+    }, 1000);
+  }
+
   return (
     <div className="flex flex-col gap-6 p-12 items-center">
       <canvas
